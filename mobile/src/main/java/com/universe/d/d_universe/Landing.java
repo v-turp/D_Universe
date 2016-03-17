4bc350eb383e7a73b913774550191ad978fb224b
victory.turpin <vturp@yahoo.com>
2016-03-16 21:27:54 -0700

    Created a landing page with the floating action button. These updates include version checking for Material Design backward compatibility.

--- mobile/src/main/java/com/universe/d/d_universe/Landing.java
+++ mobile/src/main/java/com/universe/d/d_universe/Landing.java
@@ -9,14 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class UniversalActivity extends AppCompatActivity {
+public class Landing extends AppCompatActivity {
 
     @Bind(R.id.fab) FloatingActionButton fab;
     @Bind(R.id.toolbar) Toolbar toolbar;
@@ -26,8 +24,9 @@ public class UniversalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //-- set window transitions
         getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
-        setContentView(R.layout.activity_universal);
+        setContentView(R.layout.activity_landing);
         ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // check the build version before applying the transition
@@ -36,34 +35,16 @@ public class UniversalActivity extends AppCompatActivity {
             getWindow().setExitTransition(new Explode());
         }
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getBaseContext(), Landing.class);
+                Intent intent = new Intent(getBaseContext(), UniversalActivity.class);
                 startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_universal, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
