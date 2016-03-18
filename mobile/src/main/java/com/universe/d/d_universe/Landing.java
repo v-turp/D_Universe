14af35fa6f775d981333c8f4df24d0451037d5bc
victory.turpin <vturp@yahoo.com>
2016-03-17 20:21:53 -0700

    This allows for full screen...hiding the clock etc for version 4.1 (API level 16) and higher. This is on the Landing page(the landing page should be renamed to Splash)

--- mobile/src/main/java/com/universe/d/d_universe/Landing.java
+++ mobile/src/main/java/com/universe/d/d_universe/Landing.java
@@ -3,21 +3,15 @@ package com.universe.d.d_universe;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.transition.Explode;
 import android.view.View;
 import android.view.Window;
 
-import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class Landing extends AppCompatActivity {
 
-    @Bind(R.id.fab) FloatingActionButton fab;
-    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,14 +20,22 @@ public class Landing extends AppCompatActivity {
         getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_landing);
         ButterKnife.bind(this);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         // check the build version before applying the transition
-        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             getWindow().setEnterTransition(new Explode());
             getWindow().setExitTransition(new Explode());
         }
+
+        /**
+         * this enables fullscreen for version 4.1+
+         * set view hidden step 1
+         * */
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // --- end fullscreen impl 4.1+
 
         // set the timing for the page to be displayed
         new View(this).postDelayed(new Runnable() {
