4ce0825c376845a0b8ae45d51b24ea5745e513ef
victory.turpin <vturp@yahoo.com>
2016-03-24 08:14:47 -0700

    Added initial code to dynamically add fragments. Added some version checking modifications. Other changes are likely to changes because thery were there for testing.

--- mobile/src/main/java/com/universe/d/d_universe/LandingActivity.java
+++ mobile/src/main/java/com/universe/d/d_universe/LandingActivity.java
@@ -1,5 +1,8 @@
 package com.universe.d.d_universe;
 
+import android.animation.Animator;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -9,37 +12,56 @@ import android.transition.Explode;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
+/**
+ * FragmentActivity is a special activity provided in the Support Library to handle
+ * fragments on system versions older than API level 11. If the lowest system version you support
+ * is API level 11 or higher, then you can use a regular Activity.
+ */
 public class LandingActivity extends AppCompatActivity {
 
-//    @Bind(R.id.fab) FloatingActionButton fab;
-    FloatingActionButton iv;
+    @Bind(R.id.fab) FloatingActionButton fab;
+    @Bind(R.id.ivCircularReveal) ImageView ivCircularReveal;
+//    FloatingActionButton iv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //-- set window transitions
-        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
+        }
         setIncomingTransition();
         setContentView(R.layout.activity_landing);
         ButterKnife.bind(this);
 
         // check the build version before applying the transition
-        if(Build.VERSION.SDK_INT == Build.VERSION_CODES.LOLLIPOP){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
             getWindow().setEnterTransition(new Explode());
             getWindow().setExitTransition(new Explode());
         }
 
-        setFloatingActionButtonConfig(iv);
+        setFloatingActionButtonConfig();
+        addCircularRevealAnimation(ivCircularReveal);
 
         /**
          * NOTE: Fragment Transactions...
         *If your activity allows the fragments to be removed and replaced,
         *you should add the initial fragment(s) to the activity during the activity's onCreate() method.
         */
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        GearFrag gearFrag = new GearFrag();
+        ft.add(R.id.flGearFragmentContainer, gearFrag).commit();
+
     }
 
     @Override
@@ -68,7 +90,7 @@ public class LandingActivity extends AppCompatActivity {
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 
-    private void setFloatingActionButtonConfig( FloatingActionButton fab){
+    private void setFloatingActionButtonConfig( /*FloatingActionButton fab*/){
        if(fab != null) {
            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -85,4 +107,19 @@ public class LandingActivity extends AppCompatActivity {
 
        }
     } // end method setFloatingActionButtonConfig
+
+    private void addCircularRevealAnimation(View view){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            int vx = view.getHeight() / 2;
+            int vy = view.getWidth() / 2;
+
+            float finalRadius = (float) Math.hypot(vx, vy);
+
+            Animator animation = ViewAnimationUtils.createCircularReveal(view, vx, vy,0, finalRadius);
+            animation.start();
+        }else{
+            return;
+        }
+    }
 }
